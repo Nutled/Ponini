c568b7ffa479f9fa819dc5b0ead695d7455d1ed5
Viнt@rь <vihtarb@gmail.com>
2020-10-21 19:37:21 +0300

    Implemented test request in API Added DI implementations

--- app/src/main/java/com/game/ponini/api/API.kt
+++ app/src/main/java/com/game/ponini/api/API.kt
@@ -1,8 +1,13 @@
 package com.game.ponini.api
 
+import retrofit2.Call
+import retrofit2.http.POST
+
 /**
  * Created by Viнt@rь on 21.10.2020
  */
 interface API {
 
+    @POST("/test")
+    fun test(request: Any): Call<Any>
 }
---
+++ app/src/main/java/com/game/ponini/di/AppModule.kt
@@ -0,0 +1,40 @@
+package com.game.ponini.di
+
+import com.game.ponini.BuildConfig
+import com.game.ponini.api.API
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Created by Viнt@rь on 25.09.2020
+ */
+@InstallIn(ApplicationComponent::class)
+@Module
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideAPI() : API {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        }
+
+        val client = OkHttpClient.Builder()
+            .addNetworkInterceptor(loggingInterceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(API::class.java)
+    }
+}
